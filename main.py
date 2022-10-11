f48c96d127678b9b78e0109c473c368aeafb4ebd
Dragos Sandu <sdragosflorian@gmail.com>
2022-10-11 23:10:51 +0300

    Setup for development

--- main.py
+++ main.py
@@ -8,11 +8,7 @@ import DataSets
 
 train_features, train_labels = next(iter(DataSets.train_dataloader))
 
-img = torch.permute(train_features[0].squeeze(), (1, 2, 0))
-label = train_labels[0]
-print(train_features)
-print(train_labels)
-plt.imshow(img, cmap="gray")
-plt.show()
-print(f"Label: {label}")
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print(f"Using {device} device")
+
 
