3fef85d1fb268549c0ca376bcac56d400ff413ff
Dragos Sandu <sdragosflorian@gmail.com>
2022-10-19 12:17:12 +0300

    Toned down neurons count

--- networks/EmotionsNetwork.py
+++ networks/EmotionsNetwork.py
@@ -6,11 +6,11 @@ class EmotionsNetwork(nn.Module):
         super(EmotionsNetwork, self).__init__()
         self.flatten = nn.Flatten()
         self.linear_relu_stack = nn.Sequential(
-            nn.Linear(369 * 496 * 4, 512),
+            nn.Linear(369 * 496 * 4, 256),
             nn.ReLU(),
-            nn.Linear(512, 128),
+            nn.Linear(256, 64),
             nn.ReLU(),
-            nn.Linear(128, 6)
+            nn.Linear(64, 6)
         )
 
     def forward(self, x):
